6cb0f83647099c0ac6f2ef4c90013b674564fba0
Plana-EpicTankCommander <zszht2005@gmail.com>
2026-04-26 21:59:49 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Article from '../views/Article.vue'
 import Image from '../views/Image.vue'
@@ -34,7 +34,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
